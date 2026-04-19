e94234f3faba4bfea7c6600c7783af584aea07fb
yacine-kerrad <yacinekerrad@gmail.com>
2026-04-19 15:49:40 +0000

    test: add unit tests

--- tests/training/test_main.py
+++ tests/training/test_main.py
@@ -3,5 +3,19 @@ from titanic.training.main import workflow
 
 
 def test_workflow_runs_all_steps():
-    # TODO : Implémenter le test unitaire
-    assert False
+    with (
+        patch("titanic.training.main.load_data") as mock_load,
+        patch("titanic.training.main.split_train_test") as mock_split,
+        patch("titanic.training.main.train") as mock_train,
+        patch("titanic.training.main.validate"),
+    ):
+
+        mock_load.return_value = "data.csv"
+        mock_split.return_value = ("x_train.csv", "x_test.csv", "y_train.csv", "y_test.csv")
+        mock_train.return_value = "model.joblib"
+
+        workflow("input.csv", n_estimators=10, max_depth=5, random_state=42)
+
+        mock_load.assert_called_once()
+        mock_split.assert_called_once()
+        mock_train.assert_called_once()
