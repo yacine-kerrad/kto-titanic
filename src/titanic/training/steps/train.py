906eab147656b933d61af9b2cfc6309360ae2c1f
yacine-kerrad <yacinekerrad@gmail.com>
2026-04-25 16:20:30 +0000

    fix: update test to use pickle instead of joblib

--- src/titanic/training/steps/train.py
+++ src/titanic/training/steps/train.py
@@ -1,13 +1,13 @@
 import logging
+import pickle
+import tempfile
 from pathlib import Path
-import tempfile # Nouvel import pour gérer les fichiers temporaires
 
-import joblib
-import mlflow # Nouvel import pour mlflow
+import mlflow
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-import pickle
-client = mlflow.MlflowClient() # Client mlflow    pour interagir avec le server de tracking
+
+client = mlflow.MlflowClient()
 
 ARTIFACT_PATH = "model_trained"
 
@@ -15,10 +15,10 @@ ARTIFACT_PATH = "model_trained"
 def train(x_train_path: str, y_train_path: str, n_estimators: int, max_depth: int, random_state: int) -> str:
     logging.warning(f"train {x_train_path} {y_train_path}")
     x_train = pd.read_csv(
-        client.download_artifacts(run_id=mlflow.active_run().info.run_id, path=x_train_path), index_col=False # Téléchargement des données depuis mlflow
+        client.download_artifacts(run_id=mlflow.active_run().info.run_id, path=x_train_path), index_col=False
     )
     y_train = pd.read_csv(
-        client.download_artifacts(run_id=mlflow.active_run().info.run_id, path=y_train_path), index_col=False # Téléchargement des données depuis mlflow
+        client.download_artifacts(run_id=mlflow.active_run().info.run_id, path=y_train_path), index_col=False
     )
 
     x_train = pd.get_dummies(x_train)
@@ -26,12 +26,11 @@ def train(x_train_path: str, y_train_path: str, n_estimators: int, max_depth: in
     model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, random_state=random_state)
     model.fit(x_train, y_train)
 
-    
-
     model_filename = "model.pkl"
     with tempfile.TemporaryDirectory() as tmp_dir:
         model_path = Path(tmp_dir, model_filename)
         with open(model_path, "wb") as f:
             pickle.dump(model, f)
         mlflow.log_artifact(str(model_path), ARTIFACT_PATH)
-return f"{ARTIFACT_PATH}/{model_filename}"
+
+    return f"{ARTIFACT_PATH}/{model_filename}"
